ab6758ba46fdf5573f8bfbbc83abec45e4d2c113
jwlee2560 <jwlee2560@gmail.com>
2024-05-21 17:25:21 +0900

    홈 검색 하단 슬라이드 리스트 추가 및 내비 탭 수정

--- campTeam/src/main/resources/static/js/home.js
+++ campTeam/src/main/resources/static/js/home.js
@@ -1,4 +1,4 @@
-var slideIndex = 0;
+/*var slideIndex = 0;
     showSlides();
 
     function showSlides() {
@@ -15,4 +15,19 @@ var slideIndex = 0;
         slides[slideIndex - 1].style.display = "block";
     
         setTimeout(showSlides, 10000); // 10초마다 이미지가 체인지됩니다
-    }
+    }*/
+
+$(document).ready(function(){
+      $('.post-wrapper').slick({
+        slidesToShow: 3, //한번에 보여줄 슬라이드 갯수
+        slidesToScroll: 1, //한번에 넘겨지는 슬라이드 갯수
+		autoplay: true,
+		autoplaySpeed: 4000,
+        arrows: true, //넘기기 버튼 화살표 여부
+        dots:true, //네비게이션 버튼 dot유형
+        asNavFor: '.terms-nav', //함께 움직이는 슬라이드 메뉴영역
+        infinite: false, //무한반복 옵션
+        adaptiveHeight: true, // 슬라이더 높이를 현재 슬라이드에 맞춤
+        draggable:true //드래그 가능여부
+      });
+});
