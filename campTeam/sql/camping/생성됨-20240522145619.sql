08b13c1b340ba93fddee7cd6bdc1d52d99b30fab
baroni5609 <won5609@naver.com>
2024-05-22 15:31:30 +0900

    수수정

--- campTeam/sql/camping/생성됨-20240522145619.sql
+++
@@ -1,7 +0,0 @@
---------------------------------------------------------
---  파일이 생성됨 - 수요일-5월-22-2024   
---------------------------------------------------------
-@C:\Users\Admin\Desktop\DATA\camp\CAMP_INFO_TBL.sql
---@C:\Users\Admin\Desktop\DATA\camp\CAMP_INFO_TBL_DATA_TABLE.csv
-@C:\Users\Admin\Desktop\DATA\camp\CAMP_INFO_TBL_PK.sql
-@C:\Users\Admin\Desktop\DATA\camp\CAMP_INFO_TBL_CONSTRAINT.sql
--- campTeam/sql/camping1/생성됨-20240522152854.sql
+++ campTeam/sql/camping1/생성됨-20240522152854.sql
@@ -1,5 +1,5 @@
 --------------------------------------------------------
 --  파일이 생성됨 - 수요일-5월-22-2024   
 --------------------------------------------------------
-@C:\Users\Admin\Desktop\DATA\camp\CAMPING_GROUNDS_TABLE.sql
---@C:\Users\Admin\Desktop\DATA\camp\CAMPING_GROUNDS_TABLE_DATA_TABLE.csv
+@C:\Users\Admin\Desktop\DATA\camping\CAMPING_GROUNDS_TABLE.sql
+--@C:\Users\Admin\Desktop\DATA\camping\CAMPING_GROUNDS_TABLE_DATA_TABLE.csv
